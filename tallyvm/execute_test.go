7381c1aaf63f8b3d3de069b5bb04f2939e35f59f
gluax <16431709+gluax@users.noreply.github.com>
2025-01-14 16:25:31 -0800

    chore(go): update libs

--- tallyvm/execute_test.go
+++ tallyvm/execute_test.go
@@ -128,3 +128,33 @@ func TestTallyMaxBytesExceeded(t *testing.T) {
 	assert.NotEmpty(t, res.Stdout)
 	assert.Equal(t, uint64(5002255745075), res.GasUsed)
 }
+
+func TestDrMaxBytesExceededIsFine(t *testing.T) {
+	defer cleanup()
+	tallyvm.TallyMaxBytes = 1
+
+	file := "../tally.wasm"
+	data, err := os.ReadFile(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reveals := "[{\"salt\":[1],\"exit_code\":0,\"gas_used\":\"200\",\"reveal\":[2]},{\"salt\":[3],\"exit_code\":0,\"gas_used\":\"201\",\"reveal\":[5]},{\"salt\":[4],\"exit_code\":0,\"gas_used\":\"202\",\"reveal\":[6]}]"
+	reveals_filter := "[0,0,0]"
+
+	res := tallyvm.ExecuteTallyVm(data, []string{"input_here", reveals, reveals_filter}, map[string]string{
+		"CONSENSUS":          "true",
+		"VM_MODE":            "dr",
+		"DR_TALLY_GAS_LIMIT": "300000000000000",
+	})
+
+	t.Log(res)
+
+	assert.Equal(t, "Ok", res.ExitInfo.ExitMessage)
+	assert.Equal(t, 0, res.ExitInfo.ExitCode)
+	assert.NotEmpty(t, res.Result)
+	assert.NotZero(t, res.ResultLen)
+	assert.Empty(t, res.Stderr)
+	assert.Empty(t, res.Stdout)
+	assert.Equal(t, uint64(5000128688570), res.GasUsed)
+}
