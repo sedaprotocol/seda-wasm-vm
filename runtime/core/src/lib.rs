804ce51369407ac9f54c67d5c24eede84f0af30d
gluax <16431709+gluax@users.noreply.github.com>
2025-07-01 09:00:29 -0700

    feat: cache version invalidation + tempdir fixes

--- runtime/core/src/lib.rs
+++ runtime/core/src/lib.rs
@@ -2,10 +2,9 @@ mod context;
 mod core_vm_imports;
 mod errors;
 
-pub mod metering;
-// not used with singlepass compiler
-// mod resources_dir;
 mod memory;
+pub mod metering;
+mod resources_dir;
 mod runtime;
 mod runtime_context;
 mod safe_wasi_imports;
@@ -20,6 +19,39 @@ pub const WASMER_VERSION: &str = env!("WASMER_VERSION");
 pub const WASMER_TYPES_VERSION: &str = env!("WASMER_TYPES_VERSION");
 pub const WASMER_MIDDLEWARES_VERSION: &str = env!("WASMER_MIDDLEWARES_VERSION");
 pub const WASMER_WASIX_VERSION: &str = env!("WASMER_WASIX_VERSION");
+const VERSION_FILE_NAME: &str = concat!(
+    env!("WASMER_VERSION"),
+    "-",
+    env!("WASMER_TYPES_VERSION"),
+    "-",
+    env!("WASMER_MIDDLEWARES_VERSION"),
+    "-",
+    env!("WASMER_WASIX_VERSION")
+);
+
+// non-test build: pure const fn with no allocation
+#[cfg(not(feature = "test-utils"))]
+pub const fn get_version_file_name() -> &'static str {
+    VERSION_FILE_NAME
+}
+
+#[cfg(feature = "test-utils")]
+mod test_override {
+    use std::sync::Mutex;
+
+    use super::VERSION_FILE_NAME;
+
+    // allows overwriting on each call
+    static TEST_OVERRIDE: Mutex<Option<&'static str>> = Mutex::new(None);
+
+    pub fn get_version_file_name() -> &'static str {
+        TEST_OVERRIDE.lock().unwrap().unwrap_or(VERSION_FILE_NAME)
+    }
+
+    pub fn set_test_version_file_name(val: &'static str) {
+        *TEST_OVERRIDE.lock().unwrap() = Some(val);
+    }
+}
 
 pub use context::VmContext;
 pub use core_vm_imports::create_custom_core_imports;
@@ -28,6 +60,8 @@ pub use runtime::start_runtime;
 pub use runtime_context::RuntimeContext;
 pub use safe_wasi_imports::*;
 pub use seda_runtime_sdk::{VmCallData, VmResult};
+#[cfg(feature = "test-utils")]
+pub use test_override::*;
 use tracing::level_filters::LevelFilter;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::{fmt, prelude::__tracing_subscriber_SubscriberExt, EnvFilter};
--- runtime/core/src/wasm_cache.rs
+++ runtime/core/src/wasm_cache.rs
@@ -1,49 +1,90 @@
+use std::{
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+};
+
+use wasmer::{Module, Store};
+
+use crate::{
+    errors::{Result, VmHostError},
+    get_version_file_name,
+    resources_dir::resources_home_dir,
+};
+
 pub const WASM_CACHE_FOLDER_NAME: &str = "wasm_cache";
 
-// not used with singlepass compiler
-// fn create_cache_path<ID: ToString>(sedad_home: &Path, id: ID) -> Result<PathBuf> {
-//     let mut wasm_cache_path = resources_home_dir(sedad_home);
-//     wasm_cache_path.push(WASM_CACHE_FOLDER_NAME);
+fn create_cache_path(sedad_home: &Path, id: &str) -> Result<PathBuf> {
+    let wasm_cache_path = resources_home_dir(sedad_home).join(WASM_CACHE_FOLDER_NAME).join(id);
 
-//     std::fs::create_dir_all(&wasm_cache_path)?;
-//     wasm_cache_path.push(id.to_string());
+    if !wasm_cache_path.exists() {
+        std::fs::create_dir_all(&wasm_cache_path)?;
+    }
 
-//     Ok(wasm_cache_path)
-// }
+    if wasm_cache_path.exists() && !wasm_cache_path.is_dir() {
+        Err(VmHostError::InvalidCachePath(wasm_cache_path.display().to_string()))?;
+    }
+
+    Ok(wasm_cache_path.join(get_version_file_name()))
+}
+
 pub fn wasm_cache_id<T: AsRef<[u8]>>(wasm_binary: T) -> String {
     seahash::hash(wasm_binary.as_ref()).to_string()
 }
 
-// not used with singlepass compiler
-// pub fn wasm_cache_store<ID: ToString, T: AsRef<[u8]>>(
-//     sedad_home: &Path,
-//     store: &Store,
-//     id: ID,
-//     wasm_binary: T,
-// ) -> Result<Module> {
-//     let wasm_cache_path = create_cache_path(sedad_home, id)?;
-//     let module = Module::new(&store, &wasm_binary)?;
+pub fn get_full_wasm_path_from_id(sedad_home: &Path, id: &str) -> Option<PathBuf> {
+    let wasm_cache_dir = resources_home_dir(sedad_home).join(WASM_CACHE_FOLDER_NAME).join(id);
 
-//     let mut file = File::create(wasm_cache_path)?;
-//     let buffer = module.serialize()?;
-//     file.write_all(&buffer)?;
+    if !wasm_cache_dir.exists() {
+        return None;
+    }
 
-//     Ok(module)
-// }
+    // We maintain this such that there is only one file in the directory
+    // and it is the wasm binary we are looking for.
+    let mut entries = std::fs::read_dir(&wasm_cache_dir).ok()?;
+    let entry = entries.next()?.ok()?;
+    let path = entry.path();
 
-// not used with singlepass compiler
-// pub fn wasm_cache_load<ID: ToString>(sedad_home: &Path, store: &Store, id: ID) -> Result<Module> {
-//     let wasm_cache_path = create_cache_path(sedad_home, id)?;
+    Some(path)
+}
 
-//     unsafe {
-//         let ret = Module::deserialize_from_file(&store, wasm_cache_path.clone());
+pub fn valid_wasm_cache_id(wasm_cache_path: &Path) -> bool {
+    if !wasm_cache_path.ends_with(get_version_file_name()) {
+        return false;
+    }
 
-//         if ret.is_err() {
-//             // If an error occurs while deserializing then we can not trust it anymore
-//             // so delete the cache file
-//             let _ = std::fs::remove_file(wasm_cache_path);
-//         }
+    true
+}
 
-//         Ok(ret?)
-//     }
-// }
+pub fn wasm_cache_store<T: AsRef<[u8]>>(
+    sedad_home: &Path,
+    compile_store: &Store,
+    store: &Store,
+    id: &str,
+    wasm_binary: T,
+) -> Result<Module> {
+    let wasm_cache_path = create_cache_path(sedad_home, id)?;
+    let module = Module::new(&compile_store, &wasm_binary)?;
+
+    let mut file = File::create(&wasm_cache_path)?;
+    let buffer = module.serialize()?;
+    file.write_all(&buffer)?;
+    drop(module);
+
+    let wasm_module = unsafe { Module::deserialize_from_file(&store, &wasm_cache_path)? };
+    Ok(wasm_module)
+}
+
+pub fn wasm_cache_load(store: &Store, wasm_cache_path: &Path) -> Result<Module> {
+    unsafe {
+        let ret = Module::deserialize_from_file(&store, wasm_cache_path);
+
+        if ret.is_err() {
+            // If an error occurs while deserializing then we can not trust it anymore
+            // so delete the cache file
+            let _ = std::fs::remove_file(wasm_cache_path);
+        }
+
+        Ok(ret?)
+    }
+}
